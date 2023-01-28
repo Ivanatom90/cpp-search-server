5bf861afe219f95b0b648551a31df9a51cd1bc93
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-01-28 14:48:55 +0300

    Create request_queue.cpp

---
+++ search-server/request_queue.cpp
@@ -0,0 +1,46 @@
+#include "request_queue.h"
+
+using namespace std;
+
+vector<Document> RequestQueue::AddFindRequest (const string& raw_query, DocumentStatus status) {
+    // напишите реализацию
+         if (requests_.size()==min_in_day_){
+            if(requests_.front().documents_find.size() == 0 ){
+                --empty_request_counter;
+            }
+          requests_.pop_front();
+        }
+
+        vector <Document> vector_result = search_server_.FindTopDocuments(raw_query, status);
+        requests_.push_back({raw_query, vector_result});
+        if(vector_result.size() == 0){
+            ++empty_request_counter;
+        }
+
+ return vector_result;
+}
+
+vector<Document> RequestQueue::AddFindRequest(const string& raw_query) {
+    // напишите реализацию
+           if (requests_.size()==min_in_day_){
+            if(requests_.front().documents_find.size() == 0 ){
+                empty_request_counter--;
+                  //  cout<<empty_request_counter<<"    ";
+            }
+          requests_.pop_front();
+        }
+
+    vector <Document> vector_result = search_server_.FindTopDocuments(raw_query);
+    requests_.push_back({raw_query, vector_result});
+    if(vector_result.size()==0){
+        empty_request_counter++;
+        //cout<<empty_request_counter<<"     "<<vector_result.size()<<endl;
+    }
+return vector_result;
+}
+
+
+int RequestQueue::GetNoResultRequests() const {
+    return empty_request_counter;
+}
+
