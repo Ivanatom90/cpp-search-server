7b6d4abc01a7551b43b092d826ddb60166b1752f
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-01-31 14:01:12 +0300

    Update read_imput_functions.h

--- search-server/read_imput_functions.h
+++ search-server/read_imput_functions.h
@@ -10,18 +10,3 @@
 std::string ReadLine();
 
 int ReadLineWithNumber();
-
-
-template <typename Container>
- auto Paginate  (const Container& c, int page_size) {
-    return Paginator(c.begin(), c.end(), page_size);
-}
-
-template <typename Iterator>
-ostream& operator<<(ostream& out, const IteratorRange<Iterator> iterator_type) {
-    for (auto it = iterator_type.begin(); it != iterator_type.end(); it++){
-        Document doc = *it;
-        out<<"{ document_id = "<<doc.id<<", relevance = "<<doc.relevance<<", rating = "<<doc.rating<<" }";
-}
-    return out;
-}
