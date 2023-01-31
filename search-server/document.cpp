d651135c29878fe86d00f20f82fd32792d89557e
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-01-31 23:55:23 +0300

    Create document.cpp

---
+++ search-server/document.cpp
@@ -0,0 +1,13 @@
+#include "document.h"
+
+using namespace std::literals;
+
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+
+    out << "{ " << "document_id = " << document.id << ", " << "relevance = " << document.relevance << ", "
+
+        << "rating = " << document.rating << " }";
+
+    return out;
+
+}
