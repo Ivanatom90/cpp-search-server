c264ed540f17e8cfced3807ff451e12a0b4e286c
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-01-31 23:56:21 +0300

    Update request_queue.cpp

--- search-server/request_queue.cpp
+++ search-server/request_queue.cpp
@@ -44,23 +44,4 @@ int RequestQueue::GetNoResultRequests() const {
     return empty_request_counter;
 }
 
-template <typename DocumentPredicate>
- std::vector<Document> RequestQueue::AddFindRequest(const  std::string& raw_query, DocumentPredicate document_predicate) {
-        if (requests_.size()==min_in_day_){
-           if(requests_.front().documents_find.size() == 0 ){
-                --empty_request_counter;
-            }
-          requests_.pop_front();
-        }
 
-     std::vector <Document> vector_result = search_server_.FindTopDocuments(raw_query, document_predicate);
-    requests_.push_back({raw_query, vector_result});
-    if (vector_result.size() == 0){
-        ++empty_request_counter;
-    }
-
-    return vector_result;
-
-
-    // напишите реализацию
-}
