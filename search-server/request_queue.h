bb8818160e904a87139c6ab4b9118f29e5066e77
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-01-31 14:01:56 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -9,25 +9,7 @@ public:
     }
     // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-     std::vector<Document> AddFindRequest(const  std::string& raw_query, DocumentPredicate document_predicate) {
-            if (requests_.size()==min_in_day_){
-               if(requests_.front().documents_find.size() == 0 ){
-                    --empty_request_counter;
-                }
-              requests_.pop_front();
-            }
-
-         std::vector <Document> vector_result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        requests_.push_back({raw_query, vector_result});
-        if (vector_result.size() == 0){
-            ++empty_request_counter;
-        }
-
-        return vector_result;
-
-
-        // напишите реализацию
-    }
+     std::vector<Document> AddFindRequest(const  std::string& raw_query, DocumentPredicate document_predicate);
 
 
      std::vector<Document> AddFindRequest (const  std::string& raw_query, DocumentStatus status);
@@ -52,3 +34,5 @@ private:
     int empty_request_counter;
     // возможно, здесь вам понадобится что-то ещё
 };
+
+
