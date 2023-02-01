b858e2a6138847e18548b584844dd728de07db97
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-02-01 21:04:07 +0300

    Update document.cpp

--- search-server/document.cpp
+++ search-server/document.cpp
@@ -1,13 +1,10 @@
 #include "document.h"
 
-using namespace std::literals;
+//using namespace std::literals;
+//using namespace literals;
+using namespace std;
 
-std::ostream& operator<<(std::ostream& out, const Document& document) {
-
-    out << "{ " << "document_id = " << document.id << ", " << "relevance = " << document.relevance << ", "
-
-        << "rating = " << document.rating << " }";
-
-    return out;
-
-}
+std::ostream& operator<<(std::ostream& out, const Document& document) { 
+    out << "{ " << "document_id = " << document.id << ", " << "relevance = " << document.relevance << ", " << "rating = " << document.rating << " }"; 
+    return out; 
+} 
