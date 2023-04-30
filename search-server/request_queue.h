f1a84d7eed6b64d581435891f608f586874a62ab
Ivanatom90 <115657086+Ivanatom90@users.noreply.github.com>
2023-04-30 22:02:45 +0300

    Update request_queue.h

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -7,24 +7,22 @@ class RequestQueue {
 public:
     explicit RequestQueue( SearchServer& search_server) : search_server_(search_server),  empty_request_counter(0){
     }
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-     std::vector<Document> AddFindRequest(const  std::string& raw_query, DocumentPredicate document_predicate);
+     std::vector<Document> AddFindRequest(const  std::string_view& raw_query, DocumentPredicate document_predicate);
 
 
-     std::vector<Document> AddFindRequest (const  std::string& raw_query, DocumentStatus status);
+     std::vector<Document> AddFindRequest (const  std::string_view& raw_query, DocumentStatus status);
 
 
-     std::vector<Document> AddFindRequest(const  std::string& raw_query);
+     std::vector<Document> AddFindRequest(const  std::string_view& raw_query);
 
 
     int GetNoResultRequests() const;
 
 private:
     struct QueryResult {
-         std::string query;
+         std::string_view query;
          std::vector <Document> documents_find;
-        // определите, что должно быть в структуре
     };
      std::deque<QueryResult> requests_;
     const static int min_in_day_ = 1440;
@@ -32,12 +30,11 @@ private:
 
     SearchServer& search_server_;
     int empty_request_counter;
-    // возможно, здесь вам понадобится что-то ещё
 };
 
 
 template <typename DocumentPredicate>
- std::vector<Document> RequestQueue::AddFindRequest(const  std::string& raw_query, DocumentPredicate document_predicate) {
+ std::vector<Document> RequestQueue::AddFindRequest(const  std::string_view& raw_query, DocumentPredicate document_predicate) {
         if (requests_.size()==min_in_day_){
            if(requests_.front().documents_find.size() == 0 ){
                 --empty_request_counter;
